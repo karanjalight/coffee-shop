9f443c3c3e79c1beb278aa4e4a3683290e311735
karanjalight <karanjalight@gmail.com>
2022-09-24 20:54:38 +0300

    ggod time

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-bqerrxl5.us.auth0', // the auth0 domain prefix
-    audience: 'coffeeShop', // the audience set for the auth0 app
+    url: 'dev-bqerrxl5.us', // the auth0 domain prefix
+    audience: 'login', // the audience set for the auth0 app
     clientId: '7URTOeeGK5DFepWv2xwovzXhIDvDI1ym', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8080', // the base url of the running ionic application. 
   }
